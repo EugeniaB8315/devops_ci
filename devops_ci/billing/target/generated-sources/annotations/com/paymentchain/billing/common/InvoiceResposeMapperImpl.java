93f6130a33ac79ae07c70edaed2629573757b96e
EugeniaB8315 <eugebonillo@gmail.com>
2024-02-19 18:03:37 -0300

    archivo .jar actualizado

--- devops_ci/billing/target/generated-sources/annotations/com/paymentchain/billing/common/InvoiceResposeMapperImpl.java
+++ devops_ci/billing/target/generated-sources/annotations/com/paymentchain/billing/common/InvoiceResposeMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-02-19T13:43:09-0300",
+    date = "2024-02-19T18:01:11-0300",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 20.0.2 (Oracle Corporation)"
 )
 @Component
